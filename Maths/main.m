48f0ac244eac9a79213b8ffae03e8f32ab909348
Ali <alidahesh@yahoo.com>
2016-11-01 13:11:50 -0700

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Ali Dahesh on 2016-11-01.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
